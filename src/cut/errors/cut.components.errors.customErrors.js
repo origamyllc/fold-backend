0b84a412269caa9cf696fcb57ebdb74df378a8b4
origamyllc <prashunjaveri@gmail.com>
2016-08-15 14:54:29 -0700

    code cleanup adding new files

---
+++ src/cut/errors/cut.components.errors.customErrors.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import { createCustomError } from './cut.components.errors.errorFactory';
+
+
+export function InternalServerError(settings){
+    settings.status = 500;
+    return createCustomError( settings , InternalServerError ) ;
+}
+
+export function MongooseModelDoesNotExist(){
+    let settings = {
+        status : 422,
+        errorCode :  2200,
+        message : "Mongoose model does not exist ",
+        type : "Database Error",
+        details : {
+            database: "Mongo"
+        }
+    };
+    return createCustomError( settings , MongooseModelDoesNotExist ) ;
+}
